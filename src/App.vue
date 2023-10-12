c7a10e59a7f36ee87393c714578189c621ece395
ChoSungHo <cho132@linecorp.com>
2023-10-12 15:01:24 +0900

    First init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,180 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li v-if="step == 1" @click="step++">Next</li>
+      <li v-if="step == 2" @click="publish">Publish</li>
+    </ul>
+    <img src="./assets/logo.png" alt="" class="logo" />
+  </div>
+
+  <ContainerWrap :posts="instaData" :steps="step" :url="img" />
+
+  <button v-if="step == 0" @click="more">더보기 {{ count }}</button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input @change="upload" type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContainerWrap from "./components/ContainerWrap.vue";
+import instaData from "./assets/instaData.js";
+import axios from "axios";
+
+axios.post();
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      instaData: instaData,
+      count: 0,
+      step: 0,
+      img: "",
+    };
+  },
+  methods: {
+    more() {
+      console.log(this.count);
+      axios
+        .get(`https://codingapple1.github.io/vue/more${this.count}.json`)
+        .then((result) => {
+          // 요청성공시 실행할 코드
+          this.instaData.push(result.data);
+          this.count += 1;
+        });
+    },
+    upload(e) {
+      let file = e.target.files;
+      console.log(file[0]);
+      let url = URL.createObjectURL(file[0]);
+      console.log(url);
+      this.img = url;
+      this.step += 1;
+    },
+    publish() {
+      var myPost = {
+        name: "Kim Hyun",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: `url`,
+        likes: 36,
+        date: "May 15",
+        liked: false,
+        content: "오늘 무엇을 했냐면요 아무것도 안했어요 ?",
+        filter: "perpetua",
+      };
+      this.instaData.unshift(myPost);
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    ContainerWrap,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
   margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+
+.content {
+  display: none;
+  width: 100%;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+  background-color: #ccc;
+}
+.content.on {
+  display: block;
+}
+
+.btn {
+  width: calc(100% / 3);
+  border: 0;
+  padding: 10px 20px;
+}
+.btn.is_selected {
+  background-color: #888;
+  color: #fff;
 }
 </style>
---
+++ src/components/PostWrap.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <div class="profile">
+        <img :src="props.post.userImage" alt="">
+      </div>
+      <span class="profile-name">{{ props.post.name }}</span>
+    </div>
+    <div class="post-body" :style="{ backgroundImage : `url(${props.post.postImage})` }"></div>
+    <div class="post-content">
+      <p>{{ props.post.likes }} Likes</p>
+      <p><strong>{{ props.post.name }}</strong> {{ props.post.content }}</p>
+      <p class="date">{{ props.post.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  post : Object,
+})
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  background-image: url("https://placeimg.com/100/100/arch");
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+  overflow: hidden;
+}
+.profile img{
+  width: 100%;height: 100%;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  background-image: url("https://placeimg.com/640/480/animals");
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+  overflow: hidden;
+}
+.post-body img {
+  /* width: 100%; */
+  height: 100%;
+  object-fit: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
